a44b187365eb6eadc4041bda520f8727ca133b44
AlanCali <102486125+AlanCali@users.noreply.github.com>
2022-11-06 05:58:16 -0300

    act

---
+++ app.js
@@ -0,0 +1,17 @@
+function calcular(){
+    var categoria = document.getElementById("categoria").value;
+    switch(categoria){
+        case "estudiante":
+            var descuento = 80;
+            break;
+        case "trainee":
+            var descuento = 50;
+            break;
+        case "junior":
+                var descuento = 15;
+                break;
+    }
+    var Cantidad = document.getElementById("cantidad").value;
+    var total = (Cantidad * 200)-((Cantidad*200)*descuento)/100;
+    document.getElementById("pagar").innerHTML = "Total a Pagar: $" + total;
+}
